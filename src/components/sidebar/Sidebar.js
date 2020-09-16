22d6b1c24728548f4171dc3e770613441dadcd38
jagadeshjy <jagadeshjy8898@gmail.com>
2020-09-16 11:12:06 +0530

    header changes

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SidebarRow from './SidebarRow';
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <SidebarRow/>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCYyZl3c6T3cMahubGtO9UY9fRiwHA8nys",
+    authDomain: "facebook-clone-32e4b.firebaseapp.com",
+    databaseURL: "https://facebook-clone-32e4b.firebaseio.com",
+    projectId: "facebook-clone-32e4b",
+    storageBucket: "facebook-clone-32e4b.appspot.com",
+    messagingSenderId: "471878452256",
+    appId: "1:471878452256:web:007265e438eed6a30fe845",
+    measurementId: "G-DN3ET51J7G"
+  };
