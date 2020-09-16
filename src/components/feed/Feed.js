35ac82485221c070d64fb6b61b226ce18dcd2f12
jagadeshjy <jagadeshjy8898@gmail.com>
2020-09-16 23:28:40 +0530

    sidebar changes added

---
+++ src/components/feed/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StoryReel from '../storyreel/StoryReel';
+
+function Feed() {
+    return (
+        <div className="feed">
+            <StoryReel/>
+        </div>
+    )
+}
+
+export default Feed
--- src/components/sidebar/SidebarRow.js
+++ src/components/sidebar/SidebarRow.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './SidebarRow.css';
 
-function SidebarRow() {
+function SidebarRow({src, Icon, title}) {
     return (
-        <div className="sidebarrow">
-            <p>row</p>
+        <div className="sidebarRow">
+            {src && <Avatar src={src}/>}
+            {Icon && <Icon/>}
+            <h4>{title}</h4>
         </div>
     )
 }
